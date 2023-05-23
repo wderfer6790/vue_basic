3c846c22e34af31134a6fbed26364e569cd15f3f
HSP <wderfer6709@naver.com>
2023-05-24 01:16:51 +0900

    template end

--- p03_template.js
+++ p03_template.js
@@ -17,3 +17,28 @@ var app2 = new Vue({
         no: 20,
     }
 });
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        /**
+         * 동적바인딩 html에서 camelcase를 사용했지만 
+         * "attrname" is not defined 오류발생
+         */
+        attrname: 'href',
+        url: 'http://wderfer6790.com/hsp',
+        /**
+         * 아래 methods 속성에 정의하면 찾지 못함
+         * 동적바인딩에는 제약이 많은 것 같다.
+         */
+        eventname: 'click',
+        msgAlert: function() {
+            alert('test');
+        }
+        // methods: {
+        //     msgAlert: function() {
+        //         alert('test');
+        //     }
+        // }
+    }
+});
